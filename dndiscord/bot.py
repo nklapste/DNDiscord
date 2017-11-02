ef3c4ff62123d53e995cc996f76d0f1c1907445a
nklapste <nklapste@ualberta.ca>
2017-11-02 14:38:20 -0600

    added basic dice function

--- dndiscord/bot.py
+++ dndiscord/bot.py
@@ -1,9 +1,9 @@
 """dndiscord bot api"""
 
 from logging import getLogger
-
+from random import randint
 from discord.ext import commands
-
+import re
 DESCRIPTION = """dndiscord: A Discord bot!"""  # TODO
 
 BOT = commands.Bot(command_prefix="?", description=DESCRIPTION)
@@ -22,6 +22,30 @@ async def on_message(message):
     await BOT.process_commands(message)
 
 
+class BaseCommands:
+    """General use commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def roll(self, dice: str):
+        """Rolls a dice in NdN format."""
+        try:
+            if re.match("\d+d\d+", dice):
+                rolls, limit = map(int, dice.split('d'))
+            elif re.match("d\d+", dice):
+                limit = int(dice.strip('d'))
+                rolls = 1
+        except Exception:
+            await self.bot.say('Format has to be in NdN or dN!')
+            return
+
+        result = ', '.join(str(randint(1, limit)) for _ in range(rolls))
+
+        await self.bot.say(result)
+
+
 class CharacterCreation:
     """DND character creation assistance commands"""
     pass
@@ -29,6 +53,7 @@ class CharacterCreation:
 
 class Config:
     """Config commands for the dndiscord"""
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -39,3 +64,4 @@ class Config:
 
 
 BOT.add_cog(Config(BOT))
+BOT.add_cog(BaseCommands(BOT))
